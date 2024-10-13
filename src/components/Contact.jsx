b04170b43fe69d6b6216fa8fd149270aeb78eaf9
Ewan Smith <thechosenone811@gmail.com>
2024-10-13 15:53:12 -0500

    Contact Page and More

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,21 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      this is the contact page
+        <form className="flex flex-col justify-center items-center space-y-4">
+    <div className="flex flex-col items-start">
+      <label>Name</label>
+      <input className="shadow p-2 rounded"></input>
     </div>
+    <div className="flex flex-col items-start">
+      <label>Phone Number</label>
+      <input className="shadow w-64 p-2 rounded"></input>
+    </div>
+    <div className="flex flex-col items-start">
+      <label>Message</label>
+      <input className="shadow w-64 p-2 rounded"></input>
+    </div>
+  </form>
+</div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import image from '../assets/butter.png';
 const Home = () => {
   return (
     <div className="flex items-center justify-center ">
-      <img className="shadow-lg flex items-center justify-center "src={image}></img>
+      <img className="shadow-lg flex items-center justify-center mt-8 hover:p-8"src={image}></img>
     </div>
   )
 }
